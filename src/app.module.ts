d6c787565d14c6188e1720e6574e19b12a10ba26
TenViki <viktorkomarek28@gmail.com>
2022-10-20 18:32:04 +0200

    :sparkles: Adds project structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config/dist';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DiscordModule } from './discord/discord.module';
 
-@Module({})
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+    }),
+  ],
+})
 export class AppModule {}
